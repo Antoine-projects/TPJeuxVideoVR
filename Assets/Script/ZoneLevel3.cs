115c86c01f13a5167d8f62ea949bddd042d6cbe3
Antoine Daoust <antdaoust@gmail.com>
2023-11-30 01:19:20 -0500

    Ajout du levelmanager pour le niveau 2

---
+++ Assets/Script/ZoneLevel3.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoneLevel3 : MonoBehaviour
+{
+
+    private LevelManager _levelManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _levelManager = LevelManager._Instance;
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        _levelManager.LoadAsychScene("NiveauLironShpak");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
