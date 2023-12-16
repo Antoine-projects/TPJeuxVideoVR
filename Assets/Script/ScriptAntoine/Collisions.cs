bc7d492d4e2dce9c0961c665723498ffb27bc345
Antoine Daoust <antdaoust@gmail.com>
2023-12-15 21:17:15 -0500

    Avancement de la piece

---
+++ Assets/Script/ScriptAntoine/Collisions.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Collisions : MonoBehaviour
+{
+    [SerializeField] private InfosJoueurs _infoJoueur;
+
+    [SerializeField] private Niveau _scriptNiveau;
+
+    [SerializeField] private GameObject _sonPoint;
+
+    private AudioSource _audioSource;
+
+   void Start(){
+    _audioSource = _sonPoint.GetComponent<AudioSource>();
+   } 
+
+   private void OnTriggerEnter(Collider other){
+    if(other.gameObject.tag == "Point"){
+        _infoJoueur.nbPointsMelon--;
+        Debug.Log("asfwfa");
+        Destroy(other.gameObject);
+        //_audioSource.Play();
+    }
+    else if(other.gameObject.tag == "Untagged"){
+        _infoJoueur.nbPoints--;
+        Debug.Log("asfwfa");
+        Destroy(other.gameObject);
+        //_audioSource.Play();
+    }
+   }
+}
